330a33b188adb0bf3e83ec44b0231d7d30c26fcf
mudrikam <mudrikulh@gmail.com>
2020-03-24 10:24:59 +0700

    ad social links

--- application/views/theme/header.php
+++ application/views/theme/header.php
@@ -19,3 +19,7 @@
 </head>
 
 <body>
+    <!-- livereload support -->
+    <script>
+        document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
+    </script>
